c10bd927d5bd7c1bfe565f037ede88ef4270f275
Chase Thompson <Nuclearpheasant@hotmail.com>
2017-03-13 09:50:33 -0600

    Finished Project
    Basing the final project off of the last commit, this commit will be
    able to translate to binary as long as the number is less than 16.

--- Number2Binary.py
+++ Number2Binary.py
@@ -3,9 +3,12 @@ binaryVal = [ 16, 8, 4, 2, 1]
 def main():
     binary = ['0','0','0','0','0']
     number = int(raw_input('Please input a number: '))
-    for i in xrange(4,-1,-1):
-        if(binaryVal[i] == number):
+    for i in xrange(0,5,1):
+        if(number - binaryVal[i] >= 0):
             binary[i] = '1'
+            number = number - binaryVal[i]
+        else:
+            binary[i] = '0'
     print ''.join(binary)
 
 main()
