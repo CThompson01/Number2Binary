58c99b5fdd9001bfa6c8e44b79ca66118ef3e9bf
Chase Thompson <Nuclearpheasant@hotmail.com>
2017-03-13 09:11:01 -0600

    1 Through 3
    The program can now successfully interpret numbers 1 - 3.

--- Number2Binary.py
+++ Number2Binary.py
@@ -1,9 +1,17 @@
-binary = ['0','0','0','0','1']
+binary = ['0','0','0','0','0']
 def getNumber():
+    binary = ['0','0','0','0','0']
     number = int(raw_input('Please enter a number: '))
     #print ''.join(binary)
     if(number > 1):
-        binary[4] = '0'
+        if(number < 3):
+            binary[3] = '1'
+        elif(number > 3):
+            # have another nested if statement
+            print number
+        else:
+            binary[3] = '1'
+            binary[4] = '1'
     elif(number == 1):
         binary[4] = '1'
     else:
@@ -11,3 +19,4 @@ def getNumber():
             binary[i] = '0'
     print ''.join(binary)
     
+getNumber()
